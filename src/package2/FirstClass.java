ba0b18c9ce3f43b9f9ebc7c60f869e66c9270e20
yasin koksal <yaasinkoksal@gmail.com>
2020-08-09 10:18:16 -0500

    The third commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		
 		System.out.println("This is the first entry from local master");
 		System.out.println("This is the second commit from local master");
+		System.out.println("This is the third commit from local master");
 	}
 
 }
