5a9c19c556cb6c787467dac528526886290bb149
yasinkoksal <69401298+yasinkoksal@users.noreply.github.com>
2020-08-09 10:41:36 -0500

    Update FirstClass.java
    This is the first commit from the remote master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
 		System.out.println("This is the first entry from local master");
 		System.out.println("This is the second commit from local master");
 		System.out.println("This is the third commit from local master");
+		System.out.println("This is the third commit from remote repository");
 	}
 
 }
