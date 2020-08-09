bd5e7a46bd01f013910d7334d08bce8b626549dd
yasin koksal <yaasinkoksal@gmail.com>
2020-08-09 10:12:05 -0500

    The second commit from local repo

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the first entry from local master");
+		System.out.println("This is the second commit from local master");
 	}
 
 }
